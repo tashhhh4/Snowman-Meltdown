f91b8cb1a9839ea4552c428c932117be55ec782e
Natasha Libera <tash@artoftash.com>
2026-05-20 19:10:38 +0200

    Added module docstring

--- snowman.py
+++ snowman.py
@@ -1,4 +1,22 @@
-# Snowman.py
+""" ☃️ snowman.py ☃️
+
+This is a game that can be played on the command line.
+
+If you know Hangman, you already know how this works. For the
+uninitiated, you will have to guess a word one letter at a time.
+
+But don't take too long -- Your friend, SNOWMAN, is melting!
+
+SNOWMAN needs YOU to guess the word, and fast! The word is the
+secret password to the air-conditioning unit. Only by correctly
+uncovering this secret word can you adjust the temperature of the
+room, and save your frosty friend.
+
+To play the game:
+
+    python snowman.py
+    
+"""
 
 from word_choice import WORDS
 from game_logic import print_game_title, choose_difficulty, run_game
