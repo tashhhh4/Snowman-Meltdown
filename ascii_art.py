1ef3d85c54e4776a77b8aaf9b9333d40f8ef97e2
Natasha Libera <tash@artoftash.com>
2026-05-20 18:32:29 +0200

    Developed more art frames

--- ascii_art.py
+++ ascii_art.py
@@ -9,27 +9,50 @@ FRAME_0 = """
           ( : )
           """
 
+FRAME_00 = """
+           ___
+          /___\\
+          (o o)
+          ( : )
+         _( : )_
+          """
+
 # Stage 1: Bottom part starts melting
 FRAME_1 = """
            ___
           /___\\
           (o o)
-          ( : )
+        __( : )__
+          """
+
+FRAME_10 = """
+            ___
+           /___\\
+           ( o o)
+       ___( . \\__
           """
 
 # Stage 2: Only the head remains
 FRAME_2 = """
-           ___
-          /___\\
-          (o o)
+            ___
+           /___\\
+      _____( o -)___
+          """
+
+FRAME_20 = """
+            ___
+           /___\\
+     ______( - _\\____
           """
 
 # Stage 3: Snowman completely melted
 FRAME_3 = """
-           ___
-          /___\\
+            ___
+    _______/___\\______
           """
 
 
 # List of frames => game stages
+STAGES_EASY = [FRAME_0, FRAME_00, FRAME_1, FRAME_10, FRAME_2, FRAME_20, FRAME_3]
+STAGES_MEDIUM = [FRAME_0, FRAME_1, FRAME_10, FRAME_2, FRAME_3]
 STAGES = [FRAME_0, FRAME_1, FRAME_2, FRAME_3]
