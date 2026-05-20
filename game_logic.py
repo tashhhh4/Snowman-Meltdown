1a33217f5c86dbfc6640e1a4467af691f5d8a18b
Natasha Libera <tash@artoftash.com>
2026-05-20 17:30:13 +0200

    Refactored game logic into game_logic.py

---
+++ game_logic.py
@@ -0,0 +1,60 @@
+import random
+
+
+def get_random_word(words):
+    """Selects a random word from the list."""
+    return words[random.randint(0, len(words) - 1)]
+
+
+def display_game_state(stages, mistakes, secret_word, guessed_letters):
+    """ Displays the stage of snowman melting,
+    current blanks (_) and solved letters.
+    """
+    print(stages[mistakes]) # the more mistakes, the later stage is printed
+    display_word = ""
+    for letter in secret_word:
+        if letter in guessed_letters:
+            display_word += letter + " "
+        else:
+            display_word += "_ "
+    print("Word: ", display_word)
+    print("\n")
+
+
+def run_game(snowman_stages, word_list):
+    """ Runs the display function and gets the next letter input from the user.
+        snowman_stages: A list which defines the game by providing ASCII art at every element,
+                        and setting the length of the game by the number of its elements.
+        word_list: A list of words which can be chosen at random for the game.
+    """
+    secret_word = get_random_word(word_list)
+    unique_letters = set(secret_word)
+    guessed_letters = []
+    mistakes = 0
+    max_mistakes = len(snowman_stages) - 1
+
+    print("Welcome to Snowman Meltdown!")
+
+    display_game_state(snowman_stages, mistakes, secret_word, guessed_letters)
+
+    while True:
+        guess = input("Guess a letter: ").lower()
+        print("You guessed:", guess)
+
+        # Handle guess
+        if guess in secret_word:
+            if guess not in guessed_letters:
+                guessed_letters.append(guess)
+        else:
+            mistakes += 1
+
+        # End conditions
+        if len(guessed_letters) == len(unique_letters):
+            print("You win!")
+            break
+
+        if mistakes > max_mistakes:
+            print("You lose!")
+            break        
+
+        display_game_state(snowman_stages, mistakes, secret_word, guessed_letters)
--- snowman.py
+++ snowman.py
@@ -1,7 +1,7 @@
 # Snowman.py
 
-import random
 from ascii_art import STAGES
+from game_logic import run_game
 
 
 # List of secret words
@@ -10,61 +10,7 @@ def load_words():
 
 WORDS = load_words()
 
-def get_random_word():
-    """Selects a random word from the list."""
-    return WORDS[random.randint(0, len(WORDS) - 1)]
-
-
-def display_game_state(mistakes, secret_word, guessed_letters):
-    """ Displays the stage of snowman melting,
-    current blanks (_) and solved letters.
-    """
-    print(STAGES[mistakes]) # the more mistakes, the later stage is printed
-    display_word = ""
-    for letter in secret_word:
-        if letter in guessed_letters:
-            display_word += letter + " "
-        else:
-            display_word += "_ "
-    print("Word: ", display_word)
-    print("\n")
-
-
-def play_game():
-    """ Runs the display function and gets the next letter input from the user.
-    """
-    secret_word = get_random_word()
-    unique_letters = set(secret_word)
-    guessed_letters = []
-    mistakes = 0
-    MAX_MISTAKES = 3
-
-    print("Welcome to Snowman Meltdown!")
-
-    display_game_state(mistakes, secret_word, guessed_letters)
-
-    while True:
-        guess = input("Guess a letter: ").lower()
-        print("You guessed:", guess)
-
-        # Handle guess
-        if guess in secret_word:
-            if guess not in guessed_letters:
-                guessed_letters.append(guess)
-        else:
-            mistakes += 1
-
-        # End conditions
-        if len(guessed_letters) == len(unique_letters):
-            print("You win!")
-            break
-
-        if mistakes > MAX_MISTAKES:
-            print("You lose!")
-            break        
-
-        display_game_state(mistakes, secret_word, guessed_letters)
 
 
 if __name__ == "__main__":
-    play_game()
+    run_game(STAGES, WORDS)
