921d431daa738b9ae49c30acb99a719fefe4332f
Natasha Libera <tash@artoftash.com>
2026-05-20 17:34:16 +0200

    Updated word list for easier additions.

--- snowman.py
+++ snowman.py
@@ -1,15 +1,8 @@
 # Snowman.py
 
 from ascii_art import STAGES
+from word_choice import WORDS
 from game_logic import run_game
-
-
-# List of secret words
-def load_words():
-    return ["python", "git", "github", "snowman", "meltdown"]
-
-WORDS = load_words()
-
 
 
 if __name__ == "__main__":
---
+++ word_choice.py
@@ -0,0 +1,8 @@
+word_list_raw = """python
+git
+github
+snowman
+meltdown
+"""
+
+WORDS = word_list_raw.splitlines()
