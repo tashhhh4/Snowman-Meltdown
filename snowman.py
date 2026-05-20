7b97f0674d42f44209a07bffe25094921daa66e1
Natasha Libera <tash@artoftash.com>
2026-05-20 17:08:52 +0200

    Implemented display of game state

--- snowman.py
+++ snowman.py
@@ -3,6 +3,36 @@
 import random
 
 
+# Snowman ASCII Art stages
+STAGES = [
+     # Stage 0: Full snowman
+     """
+      ___  
+     /___\\ 
+     (o o) 
+     ( : ) 
+     ( : ) 
+     """,
+     # Stage 1: Bottom part starts melting
+     """
+      ___  
+     /___\\ 
+     (o o) 
+     ( : ) 
+     """,
+     # Stage 2: Only the head remains
+     """
+      ___  
+     /___\\ 
+     (o o) 
+     """,
+     # Stage 3: Snowman completely melted
+     """
+      ___  
+     /___\\ 
+     """
+ ]
+
 # List of secret words
 def load_words():
     return ["python", "git", "github", "snowman", "meltdown"]
@@ -13,13 +43,32 @@ def get_random_word():
     """Selects a random word from the list."""
     return WORDS[random.randint(0, len(WORDS) - 1)]
 
-def play_game():
-    secret_word = get_random_word()
-    print("Welcome to Snowman Meltdown!")
-    print("Secret word selected: " + secret_word)  # for testing, later remove this line
 
-    # TODO: Build your game loop here.
-    # For now, simply prompt the user once:
+def display_game_state(mistakes, secret_word, guessed_letters):
+    """ Displays the stage of snowman melting,
+    current blanks (_) and solved letters.
+    """
+    print(STAGES[mistakes]) # the more mistakes, the later stage is printed
+    display_word = ""
+    for letter in secret_word:
+        if letter in guessed_letters:
+            display_word += letter + " "
+        else:
+            display_word += "_ "
+    print("Word: ", display_word)
+    print("\n")
+
+
+def play_game():
+    """ Runs the display function and gets the next letter input from the user.
+    """
+    secret_word = get_random_word()
+    guessed_letters = []
+    mistakes = 0
+
+    print("Welcome to Snowman Meltdown!")
+    display_game_state(mistakes, secret_word, guessed_letters)
+    
     guess = input("Guess a letter: ").lower()
     print("You guessed:", guess)
     
