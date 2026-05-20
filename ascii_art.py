df7942bed8495b59ee3fbf16d32df5a74a0d07ea
Natasha Libera <tash@artoftash.com>
2026-05-20 18:01:57 +0200

    Refactored graphics into separate frames

--- ascii_art.py
+++ ascii_art.py
@@ -1,29 +1,35 @@
-# Snowman ASCII Art stages
-STAGES = [
-     # Stage 0: Full snowman
-     """
-      ___
-     /___\\
-     (o o)
-     ( : )
-     ( : )
-     """,
-     # Stage 1: Bottom part starts melting
-     """
-      ___
-     /___\\
-     (o o)
-     ( : )
-     """,
-     # Stage 2: Only the head remains
-     """
-      ___
-     /___\\
-     (o o)
-     """,
-     # Stage 3: Snowman completely melted
-     """
-      ___
-     /___\\
-     """
- ]
+# Snowman ASCII Art frames
+
+# Stage 0: Full snowman
+FRAME_0 = """
+           ___
+          /___\\
+          (o o)
+          ( : )
+          ( : )
+          """
+
+# Stage 1: Bottom part starts melting
+FRAME_1 = """
+           ___
+          /___\\
+          (o o)
+          ( : )
+          """
+
+# Stage 2: Only the head remains
+FRAME_2 = """
+           ___
+          /___\\
+          (o o)
+          """
+
+# Stage 3: Snowman completely melted
+FRAME_3 = """
+           ___
+          /___\\
+          """
+
+
+# List of frames => game stages
+STAGES = [FRAME_0, FRAME_1, FRAME_2, FRAME_3]
